bf8a7572a7a73b5ffd6fa41d95519b64848b47d0
Kizzzy <xuhua0174@163.com>
2024-04-04 18:20:32 +0800

    [javafx-display] * reformat json file

--- src/main/java/cn/kizzzy/javafx/display/text/TextDisplay.java
+++ src/main/java/cn/kizzzy/javafx/display/text/TextDisplay.java
@@ -3,6 +3,9 @@ package cn.kizzzy.javafx.display.text;
 import cn.kizzzy.javafx.display.DisplayLoaderAttribute;
 import cn.kizzzy.vfs.IPackage;
 import cn.kizzzy.vfs.tree.Leaf;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 
 @DisplayLoaderAttribute(suffix = {
     "txt",
@@ -17,8 +20,17 @@ public class TextDisplay implements TextDisplayLoader {
     
     @Override
     public TextArg loadText(IPackage vfs, Leaf leaf) throws Exception {
+        String text = vfs.load(leaf.path, String.class);
+        if (text == null) {
+            return null;
+        }
+        
+        if (leaf.path.endsWith(".json") || leaf.path.endsWith(".JSON")) {
+            text = JSONObject.toJSONString(JSON.parse(text),
+                SerializerFeature.PrettyFormat, SerializerFeature.SortField);
+        }
         TextArg args = new TextArg();
-        args.text = vfs.load(leaf.path, String.class);
-        return args.text == null ? null : args;
+        args.text = text;
+        return args;
     }
 }
